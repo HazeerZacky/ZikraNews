0f3675e1e2962cf062767dffc3f0941914868720
Hazeer Zacky <contact.hazky@gmail.com>
2024-01-20 17:11:03 +0530

    Navebar, Splash screen and weclome screen setup

--- src/screens/SplashScreens.js
+++ src/screens/SplashScreens.js
@@ -1,10 +1,81 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect, useCallback } from "react";
+import { View, Text, Image, ImageBackground, StatusBar } from "react-native";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useNavigation } from "@react-navigation/native";
+import { LinearGradient } from "expo-linear-gradient";
 
+// SplashScreen component for displaying a splash screen before navigating to the main screen
 export default function SplashScreens() {
+  const navigation = useNavigation();
+
+  // Load custom fonts using expo-font
+  const [fontsLoaded, fontError] = useFonts({
+    SpaceGroteskSemiBold: require("../fonts/SpaceGrotesk-SemiBold.ttf"),
+    SpaceGroteskBold: require("../fonts/SpaceGrotesk-Bold.ttf"),
+    SpaceGroteskMedium: require("../fonts/SpaceGrotesk-Medium.ttf"),
+  });
+
+  // Callback function to hide the splash screen after fonts are loaded
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+
+    // Navigate to the "Welcome" screen after a delay of 3000 milliseconds (3 seconds)
+    setTimeout(() => {
+      navigation.navigate("Welcome");
+    }, 4000);
+  }, [fontsLoaded, fontError, navigation]);
+
+  // Run the onLayoutRootView callback on component mount
+  useEffect(() => {
+    onLayoutRootView();
+  }, [onLayoutRootView]);
+
+  // If fonts are not yet loaded, return null to prevent rendering
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  // Render the splash screen UI
   return (
-    <View>
-      <Text>SplashScreens</Text>
-    </View>
-  )
+    <ImageBackground
+      source={require("../../assets/images/welcome/news-bg.png")}
+      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+    >
+      {/* Gradient overlay at the bottom of the image background */}
+      <LinearGradient
+        colors={["rgba(132, 8, 11, 0.7)", "rgba(132, 8, 11, 0.7)"]}
+        style={{
+          position: 'absolute',
+          bottom: 0,
+          width: '100%',
+          height: '100%',
+        }}
+      />
+
+      {/* Main content of the splash screen */}
+      <View
+        onLayout={onLayoutRootView}
+        style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+      >
+        {/* App logo */}
+        <Image
+          source={require("../../assets/images/welcome/logo.png")}
+          style={{ width: 180, height: 180, marginBottom: -40 }}
+        />
+        
+        {/* App name */}
+        <Text style={{ color: "white", fontSize: 30, fontWeight: "bold", textTransform: "uppercase" }}>
+          Zikra News
+        </Text>
+        
+        {/* App tagline */}
+        <Text style={{ color: "white", fontSize: 10, fontWeight: "normal" }}>
+          Worlds No1 News App
+        </Text>
+      </View>
+    </ImageBackground>
+  );
 }
--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,10 +1,58 @@
-import { View, Text } from 'react-native'
+import { View, Text, ImageBackground, StatusBar, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { LinearGradient } from 'expo-linear-gradient'
+import { widthPercentageToDP } from 'react-native-responsive-screen';
+import { useNavigation } from '@react-navigation/native';
 
 export default function WelcomeScreen() {
+  const navigation = useNavigation();
+
+
+  // Hide the notification bar
+  //StatusBar.setHidden(false);
   return (
-    <View>
-      <Text>WelcomeScreen</Text>
-    </View>
+    <ImageBackground
+      source={require("../../assets/images/welcome/reportor.jpg")}
+      className="flex-1 justify-center items-center pb-6"
+    >
+      {/* Gradient overlay at the bottom of the image background */}
+      <LinearGradient
+        colors={["transparent", "rgba(132, 8, 11, 0.95)"]}
+        style={{
+          position: 'absolute',
+          bottom: 0,
+          width: '100%',
+          height: '100%',
+        }}
+      />
+
+      <View className="flex-1 items-center justify-end max-w-[85%]  space-y-4">
+        <Text
+          className="font-bold text-4xl shadow-2xl text-white text-center tracking-wider"
+          style={{
+            fontSize: widthPercentageToDP(9),
+            fontFamily: "SpaceGroteskBold",
+          }}
+        >
+          Stay Informed from Day One
+        </Text>
+        <Text
+          className="font-bold text-white text-center max-w-[85%] leading-12 tracking-wider"
+          style={{
+            fontSize: widthPercentageToDP(3),
+            fontFamily: "SpaceGroteskMedium",
+          }}
+        >
+          Discover the Latest News with our Seamless Onboarding Experience.
+        </Text>
+      </View>
+
+      <TouchableOpacity
+        className="bg-[#680104] rounded-full p-4 justify-center items-center w-[90%] mt-8"
+        onPress={() => navigation.navigate("HomeTabs")}
+      >
+        <Text className="text-base font-bold text-white">Getting Started</Text>
+      </TouchableOpacity>
+    </ImageBackground>
   )
 }
