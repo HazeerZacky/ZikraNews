99f482cd495b162102c1a10883b2af3ebc755db2
Hazeer Zacky <contact.hazky@gmail.com>
2024-01-24 08:42:09 +0530

    API Key Change. Reached Limit

--- utils/ApiKey.js
+++ utils/ApiKey.js
@@ -1,1 +1,2 @@
-export const newsApiKey = "93a04f6e577d4f9ea9c1168a8ff77c32";
+export const newsApiKey = "56d996f015b6474589a34371f7d28ea3";
+// export const newsApiKey = "93a04f6e577d4f9ea9c1168a8ff77c32";
