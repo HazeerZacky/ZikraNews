1904e59623b098de1a8abc8f2f6cb10e245b1830
Hazeer Zacky <contact.hazky@gmail.com>
2024-02-09 11:27:28 +0530

    Package Version Update and API Key Update

--- utils/ApiKey.js
+++ utils/ApiKey.js
@@ -1,2 +1,2 @@
-export const newsApiKey = "56d996f015b6474589a34371f7d28ea3";
-// export const newsApiKey = "93a04f6e577d4f9ea9c1168a8ff77c32";
+export const newsApiKey = "3e6d5a1ad3214e4aa4100989e5415a8f";
+// export const newsApiKey = "93a04f6e577d4f9ea9c1168a8ff77c32"; - old
